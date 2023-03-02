acd6840a8f1b6aa7a1e596a3176b800a34d65838
Irwan X Yans <irwan080304@gmail.com>
2023-03-02 13:51:42 +0700

    Update Latihan_1.cxx

--- maret/2_maret/Latihan_1.cxx
+++ maret/2_maret/Latihan_1.cxx
@@ -1,13 +1,12 @@
-#include <iostream>  // for std::cout and std::cin
-
-int main()
-{
-    int x{ }, y{ }, z; 
-    std::cout << "Program Menjumlahkan Angka X dan Y\n" << "Masukkan Angka X\t: "; // ask user for a number
-    std::cin >> x;  // get number from keyboard and store it in variable X
-    std::cout << "Masukkan Angka Y\t: "; // ask user for a number
-    std::cin >> y; // get number from keyboard and store it in variable Y
+#include <iostream>
+using namespace std;
+int main(){
+    int x, y, z; 
+    cout << "Menjumlahkan Angka 1 & 2\nMasukkan Angka ke-1\t: ";
+    cin >> x;
+    cout << "Masukkan Angka ke-2\t: ";
+    cin >> y;
     z=x+y;
-    std::cout << "Hasil Penjumlahan Dari " << x << " + " << y << " Adalah : " << z << "\n";
+    cout << "Hasil Penjumlahan Dari " << x << " + " << y << " Adalah : " << z;
     return 0;
 }
